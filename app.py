560c90d553b23fe02cb046c94e291056bdb45129
Dharmesh-Poddar <dharmeshpoddar3@gmail.com>
2020-04-11 18:09:17 +0530

    basic commits

--- app.py
+++ app.py
@@ -9,3 +9,23 @@ import json
 app= Flask(__name__)
 api=Api(app)
 
+client= MongoClient("mongodb://db:27017")
+db= client.ImageRecognition
+users= db["Users"]
+
+
+class Register(Resource):
+    def post(self):
+        postedData=request.get_json()
+        username= postedData["username"]
+        password= postedData["password"]
+
+        if UserNotExist(username):
+        	retJson={
+                "status":301,
+                "msg": "user do not exist"
+
+        	}
+
+            return jsonify(retJson)
+            
